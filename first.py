6284d95d0456dc46179484f677824af3022fb770
jaitavya123 <jaitavya123@gmail.com>
2024-08-19 16:15:15 +0530

    append operation

--- first.py
+++ first.py
@@ -13,9 +13,15 @@ try:
  a1.close()
  print("read mode ")
  a2 = open("start.txt", "r")
- a2.seek(3)                                  # move cursor to any desired position
- a2.tell()                                   # give current location of cursor
+ #a2.seek(3)                                  # move cursor to any desired position
+ #a2.tell()                                   # give current location of cursor
  print(a2.read())
+ a2.close()
+ print("APPEND mode ")
+ a3=open("start.txt", "a+")
+ a3.write(" appended txt")
+ a3.seek(0)
+ print("after appending ==", a3.read())
 except FileExistsError as excep1:
     print(">>>>>>>>>Exception",excep1)
 except FileNotFoundError as except2:
