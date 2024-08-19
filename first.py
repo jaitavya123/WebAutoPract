be71fc4b5b063201d7d3348fde0726bfe99e2bb9
jaitavya123 <jaitavya123@gmail.com>
2024-08-19 15:59:09 +0530

    file I/O

--- first.py
+++ first.py
@@ -1,1 +1,25 @@
-print("hello")
+print("hi")
+# exception handling
+'''try:
+    a=2/0
+    print(a)
+except ArithmeticError as d:
+    print(d)'''                               # exception handling simple example end
+
+try:
+ a1=open("start.txt","w")                    # open("start.txt","w") create an empty file
+ data=input("type somethng")                 # taking user data into files
+ a1.write(data)
+ a1.close()
+ print("read mode ")
+ a2 = open("start.txt", "r")
+ a2.seek(3)                                  # move cursor to any desired position
+ a2.tell()                                   # give current location of cursor
+ print(a2.read())
+except FileExistsError as excep1:
+    print(">>>>>>>>>Exception",excep1)
+except FileNotFoundError as except2:
+    print(">>>>>>>>>Exception",except2)
+
+
+
